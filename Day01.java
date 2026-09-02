def24f47a1d6e670ce05cdf0b6574cd8b1a8e30b
febryana-rgb <febryana210228@gmail.com>
2026-09-02 22:00:46 +0800

    Day01.java

---
+++ Day01.java
@@ -0,0 +1,10 @@
+class Day1 {
+    public static void main(String[] args) {
+        // Komentar satu baris
+         /* Komentar beberapa baris */
+          /** Komentar dokumentasi */
+        
+        System.out.println("Hello, World!");
+        System.out.println("Memahami struktur dasar kode Java & menggunakan semua jenis Komentar");
+    }
+}
