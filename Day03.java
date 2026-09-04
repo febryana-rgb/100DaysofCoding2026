e0559dae64a9409b8d529b148c7185ed720f34ae
febryana-rgb <febryana210228@gmail.com>
2026-09-04 19:46:36 +0800

    Day03.java

---
+++ Day03.java
@@ -0,0 +1,18 @@
+class Day3 {
+    public static void main(String[] args) {
+    // \n Baris Baru
+    System.out.println("HALOO\nNama saya Asipa Febriana");
+
+    // \t Memberikan jarak layaknya menekan Tab di keyboard
+    System.out.println("Prodi\t: Pendidikan Teknologi Informasi");
+
+    // \" untuk menampilkan tanda kutip ganda
+    System.out.println("INSPARTA 25! \"Menghapus batas dunia, menjelajah luas dengan Teknologi\"");
+
+    // \' untuk menampilkan tanda kutip tunggal
+    System.out.println("Siapa kita! \'TECHNOLOGIAA\'");
+    
+    // \\ untuk menampilkan garis miring terbalik
+    System.out.println("C:\\100DaysofCoding2026\\Day3");
+    }
+}
