eebf43389f28c1325b48230439d283e72fb33762
febryana-rgb <febryana210228@gmail.com>
2026-09-06 20:18:36 +0800

    Day5.java

---
+++ Day5.java
@@ -0,0 +1,11 @@
+public class Day5 {
+    public static void main(String[] args) {
+        // Tipe data float 32 bit, menyimpan 6 - 7 digit, wajib menggunakan huruf "f" pada akhir angka agar terbaca sebagai tipe data float
+        float tinggiBadan = 160.5f;
+        System.out.println("Tinggi Badan\t: " + tinggiBadan + " cm");
+
+        // Tipe data double 64 bit, menyimpan 15 - 16 digit
+        double beratBadan = 60.55;
+        System.out.println("Berat Badan\t: " + beratBadan + " kg");
+    }
+}
