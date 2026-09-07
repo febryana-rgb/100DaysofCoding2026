653a6d9629292fb1f1246ae7369e23889a13d107
febryana-rgb <febryana210228@gmail.com>
2026-09-07 20:56:59 +0800

    Day6.java

---
+++ Day6.java
@@ -0,0 +1,15 @@
+public class Day6 {
+    public static void main(String[] args) {
+    // tipe data karakter char, untuk menyimpan 1 karakter berupa huruf, angka, simbol, atau karakter lainnya yang harus diapit dengan tanda kutip tunggal
+    char golonganDarah = 'O';
+    char jenisKelamin = 'P';
+    System.out.println("Golongan Darah\t:" + golonganDarah);
+    System.out.println("Jenis Kelamin\t:" + jenisKelamin);
+
+    // tipe data karakter boolean, digunakan untuk mengetahui nilai kebenaran True atau False
+    boolean hadir = true;
+    boolean lulus = false;
+    System.out.println("Kehadiran\t:" + hadir);
+    System.out.println("Nilai UAS\t:" + lulus);
+    }
+}
