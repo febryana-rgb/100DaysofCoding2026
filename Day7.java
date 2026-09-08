c9aa59691b56cc298d3cf0e0bf9265e5b9f23f6d
febryana-rgb <febryana210228@gmail.com>
2026-09-08 20:17:10 +0800

    Day7.java

---
+++ Day7.java
@@ -0,0 +1,11 @@
+public class Day7 {
+    public static void main(String[] args) {
+    // Tipe data string menyimpan teks atau kumpulan karakter, wajib diapit tanda kutip ganda
+    String nama = "Asipa Febriana";
+    String angkatan = "2025";
+    String asalKota = "Bone";
+    System.out.println("Nama\t\t:" + nama);
+    System.out.println("Angkatan\t:" + angkatan);
+    System.out.println("Kota Asal\t:" + asalKota);
+    }
+}
