36afa42746dd0ece69affeaa6644dc71fbc179a4
febryana-rgb <febryana210228@gmail.com>
2026-09-11 20:58:24 +0800

    Day10.java

---
+++ Day10.java
@@ -0,0 +1,56 @@
+public class Day10 {
+    public static void main(String[] args) {
+    // Batas minimal dan maksimal tipe data Byte
+    // Byte.MIN_VALUE konstanta untuk menyimpan nilai minimal tipe data byte
+    // Byte.MAX_VALUE konstanta untuk menyimpan nilai maksimal tipe data byte
+    System.out.println("Batas minimal dan maksimal tipe data Byte");
+    System.out.println("Minimal\t\t:" + Byte.MIN_VALUE);
+    System.out.println("Maksimal\t:" + Byte.MAX_VALUE);
+
+    // Batas minimal dan maksimal tipe data Short
+    // Short.MIN_VALUE konstanta untuk menyimpan nilai minimal tipe data short
+    // Short.MAX_VALUE konstanta untuk menyimpan nilai maksimal tipe data short
+    System.out.println("\nBatas minimal dan maksimal tipe data Short");
+    System.out.println("Minimal\t\t:" + Short.MIN_VALUE);
+    System.out.println("Maksimal\t:" + Short.MAX_VALUE);
+
+    // Batas minimal dan maksimal tipe data int
+    // Integer.MIN_VALUE konstanta untuk menyimpan nilai minimal tipe data int
+    // Integer.MAX_VALUE konstanta untuk menyimpan nilai maksimal tipe data int
+    System.out.println("\nBatas minimal dan maksimal tipe data Int");
+    System.out.println("Minimal\t\t:" + Integer.MIN_VALUE);
+    System.out.println("Maksimal\t:" + Integer.MAX_VALUE);
+
+    // Batas minimal dan maksimal tipe data long
+    // Long.MIN_VALUE konstanta untuk menyimpan nilai minimal tipe data long
+    // Long.MAX_VALUE konstanta untuk menyimpan nilai maksimal tipe data long
+    System.out.println("\nBatas minimal dan maksimal tipe data Long");
+    System.out.println("Minimal\t\t:" + Long.MIN_VALUE);
+    System.out.println("Maksimal\t:" + Long.MAX_VALUE);
+
+    // Batas minimal dan maksimal tipe data double
+    // Double.MIN_VALUE konstanta untuk menyimpan nilai minimal tipe data double
+    // Double.MAX_VALUE konstanta untuk menyimpan nilai maksimal tipe data double
+    System.out.println("\nBatas minimal dan maksimal tipe data Double");
+    System.out.println("Minimal\t\t:" + Double.MIN_VALUE);
+    System.out.println("Maksimal\t:" + Double.MAX_VALUE);
+
+    // Batas minimal dan maksimal tipe data float
+    // Float.MIN_VALUE konstanta untuk menyimpan nilai minimal tipe data float
+    // Float.MAX_VALUE konstanta untuk menyimpan nilai maksimal tipe data float
+    System.out.println("\nBatas minimal dan maksimal tipe data Float");
+    System.out.println("Minimal\t\t:" + Float.MIN_VALUE);
+    System.out.println("Maksimal\t:" + Float.MAX_VALUE);
+
+    // Batas minimal dan maksimal tipe data char
+    // Character.MIN_VALUE konstanta untuk menyimpan nilai minimal tipe data char
+    // Character.MAX_VALUE konstanta untuk menyimpan nilai maksimal tipe data char
+    System.out.println("\nBatas minimal dan maksimal tipe data Char");
+    System.out.println("Minimal\t\t:" + Character.MIN_VALUE);
+    System.out.println("Maksimal\t:" + Character.MAX_VALUE);
+    //(int) Character.MIN_VALUE dan Character.MAX_VALUE untuk menampilkan angka dari char bukan simbolnya
+    System.out.println("\nBatas minimal dan maksimal tipe data Char, Setelah di casting ke (Int)");
+    System.out.println("Minimal\t\t:" + (int) Character.MIN_VALUE);
+    System.out.println("Maksimal\t:" + (int) Character.MAX_VALUE);
+    }
+}
